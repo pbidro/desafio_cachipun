5af9fa2207126aae0a3cad1cc1382d72167f4373
pbidro <pb.idro94@gmail.com>
2021-11-02 01:22:36 -0300

    mejoras fail

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
-
+var historial = '';
+var historial_show ='';
 function jugar() {
-
+    var cantidad_juegos = parseInt(prompt("¿Cuántos juegos quieres jugar?"));
     var jugada = ['nada', 'piedra', 'papel', 'tijera']
-    var player = prompt("Ingresa tu jugada: \n 1-piedra \n 2-papel \n 3-tijera");
-    var bot = Math.floor(Math.random() * 3) + 1;
-    
-    var resultado = '';
-    if (player == 1 && bot == 1)
-        resultado = (`bot juega ${jugada[bot]} y tu juegas ${jugada[player]} \n Empate`);
-    else if (player == 1 && bot == 2)
-        resultado = (`bot juega ${jugada[bot]} y tu juegas ${jugada[player]} \n Perdiste`);
-    else if (player == 1 && bot == 3)
-        resultado = (`bot juega ${jugada[bot]} y tu juegas ${jugada[player]} \n Ganaste`);
-    else if (player == 2 && bot == 1)
-        resultado = (`bot juega ${jugada[bot]} y tu juegas ${jugada[player]} \n Ganaste`);
-    else if (player == 2 && bot == 2)
-        resultado = (`bot juega ${jugada[bot]} y tu juegas ${jugada[player]} \n Empate`);
-    else if (player == 2 && bot == 3)
-        resultado = (`bot juega ${jugada[bot]} y tu juegas ${jugada[player]} \n Perdiste`);
-    else if (player == 3 && bot == 1)
-        resultado = (`bot juega ${jugada[bot]} y tu juegas ${jugada[player]} \n Perdiste`);
-    else if (player == 3 && bot == 2)
-        resultado = (`bot juega ${jugada[bot]} y tu juegas ${jugada[player]} \n Ganaste`);
-    else if (player == 3 && bot == 3)
-        resultado = (`bot juega ${jugada[bot]} y tu juegas ${jugada[player]} \n Empate`);
-    else
-        resultado = ("Ingresa una jugada valida");
+    var victorias = 0;
+    var derrotas = 0;
+    var empates = 0;
+    for (var i = 0; i < cantidad_juegos; i++) {
 
-    document.getElementById('resultado').innerHTML = resultado;
+        var jugada_usuario = jugada[parseInt(prompt("¿Qué eliges: 1-piedra, 2-papel, 3-tijera ?"))];
+        var jugada_pc = jugada[parseInt(Math.floor(Math.random() * jugada.length))];
+
+        if (jugada_usuario == jugada_pc) {
+            
+            historial=(" pc jugó " + jugada_pc + " y tu jugaste " + jugada_usuario + " ¡Empate!");
+            empates++;
+        } else if (jugada_usuario == "piedra" && jugada_pc == "papel") {
+
+            historial=("pc jugó " + jugada_pc + " y tu jugaste " + jugada_usuario + " ¡Gana la PC!");
+            derrotas++;
+        }
+        else if (jugada_usuario == "piedra" && jugada_pc == "tijera") {
+            historial=("pc jugó " + jugada_pc + " y tu jugaste " + jugada_usuario + " ¡Gana el usuario!");
+            victorias++;
+        }
+        else if (jugada_usuario == "papel" && jugada_pc == "piedra") {
+            historial=("pc jugó " + jugada_pc + " y tu jugaste " + jugada_usuario + " ¡Gana el usuario!");
+            victorias++;
+        }
+        else if (jugada_usuario == "tijera" && jugada_pc == "piedra") {
+            historial=("pc jugó " + jugada_pc + " y tu jugaste " + jugada_usuario + " ¡Gana la PC!");
+            derrotas++;
+        }
+        else if (jugada_usuario == "tijera" && jugada_pc == "papel") {
+            historial=("pc jugó " + jugada_pc + " y tu jugaste " + jugada_usuario + " ¡Gana el usuario!");
+            victorias++;
+        }
+        else {
+            alert("Elige una opción válida");
+        }
+        alert(historial);
+    }
 
 }
+
+
+
+
+
+
